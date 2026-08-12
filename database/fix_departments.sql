8767e1be363ac0f72d5ab10df85d93ae6a1b3835
pharmacisttom <pharmacisttom@gmail.com>
2026-08-12 19:36:21 +0700

    Fix garbled department names in DB

---
+++ database/fix_departments.sql
@@ -0,0 +1,3 @@
+UPDATE departments SET name='กลุ่มงานการแพทย์', description='รับผิดชอบด้านการตรวจรักษาโรคทั่วไปและเฉพาะทาง' WHERE id=1 OR id=4;
+UPDATE departments SET name='กลุ่มงานทันตกรรม', description='บริการด้านทันตกรรมครบวงจร' WHERE id=2 OR id=5;
+UPDATE departments SET name='กลุ่มงานเภสัชกรรม', description='บริการจ่ายยาและให้คำปรึกษาด้านยา' WHERE id=3 OR id=6;
